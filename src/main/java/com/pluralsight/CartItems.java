b409570f8c78d2eec28966e94dc38e51eaee6e2c
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 21:22:09 -0500

    added bonus properties, constructor, get/set

---
+++ src/main/java/com/pluralsight/CartItems.java
@@ -0,0 +1,36 @@
+package com.pluralsight;
+
+public class CartItems {
+    //add bonus options
+
+    //add property
+    private Product product;
+    private int quality;
+
+
+    //add constructor
+
+    public CartItems(Product product, int quality) {
+        this.product = product;
+        this.quality = 1;
+    }
+
+
+    //add get set
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+}
