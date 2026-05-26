4d3ea2cae4907793b0381185aa94fb33eb6e09b3
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 21:33:54 -0500

    added store, products csv, add arrays

--- src/main/java/com/pluralsight/Store.java
+++ src/main/java/com/pluralsight/Store.java
@@ -6,4 +6,13 @@ import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Store {
+    //add arrays
+    private ArrayList<Product> inventory = new ArrayList<>();
+    private ArrayList<CartItems> cartItems = new ArrayList<>();
+
+
+    //load the given csv with try catch
+    public void loadProducts(){
+
+    }
 }
