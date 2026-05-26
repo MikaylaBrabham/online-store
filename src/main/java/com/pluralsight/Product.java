19dcf31a9acc993e8f84bdb198af16498e4721df
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 21:13:22 -0500

    added a product class properties, constructors, get/set

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,55 @@
 package com.pluralsight;
 
 public class Product {
+
+    //add properties in given example
+    private String sku;
+    private String name;
+    private float price;
+    private String department;
+
+
+    //add constructor
+
+    public Product(String sku, String name, float price, String department) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+        this.department = department;
+    }
+
+    //add get/set
+
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
